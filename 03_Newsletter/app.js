3e9895b4ad13c3a894c39203000f88bc8719f3c9
AnsloneKritisch <digveshoptimus@gmail.com>
2023-07-18 12:38:58 +0530

    new

--- 03_Newsletter/app.js
+++ 03_Newsletter/app.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
 const port = 3000
+//  after doing it we realised that the server does not take the css static file so we are creating folder and adding all our css and images at a different folder and addind it here
+
+app.use(express.static("public"));
 
 app.get('/', function(req, res) 
 {
